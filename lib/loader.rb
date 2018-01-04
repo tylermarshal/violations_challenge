e387bea489b188ffae23684033a8da928bc980c8
TMad <madsen.tyler@gmail.com>
2018-01-04 09:47:43 -0700

    add count_of_violations

--- lib/loader.rb
+++ lib/loader.rb
@@ -2,12 +2,23 @@ require './violation'
 
 class Loader
 
+  attr_reader :violations
+
+  def initialize
+    @violations = []
+  end
+
   def load_csv
     CSV.readlines('./Violations-2012.csv', headers: true, header_converters: :symbol) do |row|
-      Violation.new(row)
+      violations << Violation.new(row)
     end
   end
 
+  def count_of_violations
+    violations.reduce(Hash.new(0)) do |violation|
+      violation.
+    end
+  end
 
 
 end
